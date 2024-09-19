79a3b702d669e93970780acabe4fe7fe85bf0806
Divya <dxn48340@ucmo.edu>
2024-09-18 21:05:46 -0400

    PayTM frontend

--- course-work/week8.2/PayTM/frontend/src/App.jsx
+++ course-work/week8.2/PayTM/frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
+      <Route path="/signin" element={<Signin></Signin>}></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+      <Route path="/send" element={<Send></Send>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
